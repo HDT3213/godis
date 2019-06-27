676797cc04f876c4fbcedad3b9e0ba7399fea152
hdt3213 <hdt3213@outlook.com>
2019-06-27 22:21:03 +0800

    refactor dict

--- src/datastruct/dict/dict.go
+++ src/datastruct/dict/dict.go
@@ -6,24 +6,36 @@ import (
 )
 
 type Dict struct {
-    shards []*Shard
-    shardCount int
-    count int32
+    table       atomic.Value // []*Shard
+    nextTable   []*Shard
+    nextTableMu sync.Mutex
+    count       int32
+
+    rehashIndex int32
+}
+
+type Node struct {
+    key      string
+    val      interface{}
+    next     *Node
+    hashCode uint32
 }
 
 type Shard struct {
-    table map[string]interface{}
+    head  *Node
     mutex sync.RWMutex
 }
 
 const (
-    maxCapacity = 1 << 16
-    minCapacity = 256
+    maxCapacity      = 1 << 15
+    minCapacity      = 16
+    rehashConcurrent = 4
+    loadFactor       = 0.75
 )
 
 // return the mini power of two which is not less than cap
 // See Hackers Delight, sec 3.2
-func computeCapacity(param int)(size int) {
+func computeCapacity(param int) (size int) {
     if param <= minCapacity {
         return minCapacity
     }
@@ -40,24 +52,24 @@ func computeCapacity(param int)(size int) {
     }
 }
 
-func Make(shardCountHint int)*Dict {
+func Make(shardCountHint int) *Dict {
     shardCount := computeCapacity(shardCountHint)
-    shards := make([]*Shard, shardCount)
+    table := make([]*Shard, shardCount)
     for i := 0; i < shardCount; i++ {
-        shards[i] = &Shard{
-            table: make(map[string]interface{}),
-        }
+        table[i] = &Shard{}
     }
-    return &Dict{
-        shards: shards,
-        shardCount: shardCount,
-        count: 0,
+    d := &Dict{
+        count:       0,
+        rehashIndex: -1,
     }
+    d.table.Store(table)
+    return d
 }
+
+const prime32 = uint32(16777619)
 
 func fnv32(key string) uint32 {
     hash := uint32(2166136261)
-    const prime32 = uint32(16777619)
     for i := 0; i < len(key); i++ {
         hash *= prime32
         hash ^= uint32(key[i])
@@ -65,92 +77,416 @@ func fnv32(key string) uint32 {
     return hash
 }
 
-
-func (d *Dict)spread(key string) int {
-    h := int(fnv32(key))
-    return (d.shardCount - 1) & h
+func (dict *Dict) spread(hashCode uint32) uint32 {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    table, _ := dict.table.Load().([]*Shard)
+    tableSize := uint32(len(table))
+    return (tableSize - 1) & uint32(hashCode)
 }
 
-func (d *Dict)Get(key string)(val interface{}, exists bool) {
-   shard := d.shards[d.spread(key)]
-   shard.mutex.RLock()
-   defer shard.mutex.RUnlock()
-
-   val, ok := shard.table[key]
-   return val, ok
+func (dict *Dict) getShard(index uint32) *Shard {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    table, ok := dict.table.Load().([]*Shard)
+    if !ok {
+        panic("load table failed")
+    }
+    return table[index]
 }
 
-func (d *Dict)Len()int {
-    return int(atomic.LoadInt32(&d.count))
+func (dict *Dict) getNextShard(hashCode uint32) *Shard {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    if dict.nextTable == nil {
+        panic("next table is nil")
+    }
+    nextTableSize := uint32(len(dict.nextTable))
+    index := (nextTableSize - 1) & uint32(hashCode)
+    return dict.nextTable[index]
+}
+
+func (dict *Dict) ensureNextTable() {
+    if dict.nextTable == nil {
+        dict.nextTableMu.Lock()
+
+        // check-lock-check
+        if dict.nextTable == nil {
+            table, _ := dict.table.Load().([]*Shard)
+            tableSize := uint32(len(table))
+            // init next table
+            nextShardCount := tableSize * 2
+            if nextShardCount > maxCapacity || nextShardCount < 0 {
+                nextShardCount = maxCapacity
+            }
+            if nextShardCount <= tableSize {
+                // reach limit, cannot resize
+                atomic.StoreInt32(&dict.rehashIndex, -1)
+                return
+            }
+            nextTable := make([]*Shard, nextShardCount)
+            var i uint32
+            for i = 0; i < nextShardCount; i++ {
+                nextTable[i] = &Shard{}
+            }
+            dict.nextTable = nextTable
+        }
+
+        dict.nextTableMu.Unlock()
+    }
+}
+
+func (shard *Shard) Get(key string) (val interface{}, exists bool) {
+    if shard == nil {
+        panic("shard is nil")
+    }
+    shard.mutex.RLock()
+    defer shard.mutex.RUnlock()
+
+    node := shard.head
+    for node != nil {
+        if node.key == key {
+            return node.val, true
+        }
+        node = node.next
+    }
+    return nil, false
+}
+
+func (dict *Dict) Get(key string) (val interface{}, exists bool) {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    hashCode := fnv32(key)
+    index := dict.spread(hashCode)
+    rehashIndex := atomic.LoadInt32(&dict.rehashIndex)
+    if rehashIndex >= int32(index) {
+        /* if rehashIndex > index. then the shard has finished resize, put in next table
+         * if rehashIndex == index, the shard may be resizing or just finished.
+         * Resizing will not be finished until the lock has been released
+         */
+        dict.ensureNextTable()
+        nextShard := dict.getNextShard(hashCode)
+        val, exists = nextShard.Get(key)
+    } else {
+        /*
+         * if rehashing not in progress or the shard has not been rehashing, put in current shard
+         */
+        shard := dict.getShard(index)
+        val, exists =  shard.Get(key)
+    }
+
+    return
+}
+
+func (dict *Dict) Len() int {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    return int(atomic.LoadInt32(&dict.count))
+}
+
+func (shard *Shard) Put(key string, val interface{}, hashCode uint32) int {
+    if shard == nil {
+        panic("shard is nil")
+    }
+    shard.mutex.Lock()
+    defer shard.mutex.Unlock()
+
+    node := shard.head
+    if node == nil {
+        // empty shard
+        node = &Node{
+            key:      key,
+            val:      val,
+            hashCode: hashCode,
+        }
+        shard.head = node
+        return 1
+    } else {
+        for {
+            if node.key == key {
+                // existed node
+                node.val = val
+                return 0
+            }
+            if node.next == nil {
+                // append
+                node.next = &Node{
+                    key:      key,
+                    val:      val,
+                    hashCode: hashCode,
+                }
+                return 1
+            }
+            node = node.next
+        }
+    }
 }
 
 // return the number of new inserted key-value
-func (d *Dict)Put(key string, val interface{})int {
-    shard := d.shards[d.spread(key)]
+func (dict *Dict) Put(key string, val interface{}) (result int) {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    hashCode := fnv32(key)
+    index := dict.spread(hashCode)
+    rehashIndex := atomic.LoadInt32(&dict.rehashIndex)
+    if rehashIndex >= int32(index) {
+        /* if rehashIndex > index. then the shard has finished resize, put in next table
+         * if rehashIndex == index, the shard may be resizing or just finished.
+         * Resizing will not be finished until the lock has been released
+         */
+        dict.ensureNextTable()
+        nextShard := dict.getNextShard(hashCode)
+        result = nextShard.Put(key, val, hashCode)
+    } else {
+        /*
+         * if rehashing not in progress or the shard has not been rehashing, put in current shard
+         */
+        shard := dict.getShard(index)
+        result = shard.Put(key, val, hashCode)
+    }
+    if result == 1 {
+        dict.addCount()
+    }
+    return result
+}
+
+func (shard *Shard) PutIfAbsent(key string, val interface{}, hashCode uint32)int {
     shard.mutex.Lock()
     defer shard.mutex.Unlock()
-
-    _, existed := shard.table[key]
-    shard.table[key] = val
-
-    if existed {
-        // update
-        return 0
-    } else {
-        // insert
-        atomic.AddInt32(&d.count, 1)
+    node := shard.head
+    if node == nil {
+        // empty shard
+        node = &Node{
+            key:      key,
+            val:      val,
+            hashCode: hashCode,
+        }
+        shard.head = node
         return 1
+    } else {
+        for {
+            if node.key == key {
+                // existed node
+                return 0
+            }
+            if node.next == nil {
+                // append
+                node.next = &Node{
+                    key:      key,
+                    val:      val,
+                    hashCode: hashCode,
+                }
+                return 1
+            }
+            node = node.next
+        }
     }
 }
 
 // return the number of updated key-value
-func (d *Dict)PutIfAbsent(key string, val interface{})int {
-    shard := d.shards[d.spread(key)]
+func (dict *Dict) PutIfAbsent(key string, val interface{}) (result int) {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    hashCode := fnv32(key)
+    index := dict.spread(hashCode)
+
+    rehashIndex := atomic.LoadInt32(&dict.rehashIndex)
+    if rehashIndex >= int32(index) {
+        dict.ensureNextTable()
+        nextShard := dict.getNextShard(hashCode)
+        result = nextShard.PutIfAbsent(key, val, hashCode)
+    } else {
+        shard := dict.getShard(index)
+        result = shard.PutIfAbsent(key, val, hashCode)
+    }
+    if result == 1 {
+        dict.addCount()
+    }
+    return result
+}
+
+func (shard *Shard) PutIfExists(key string, val interface{})int {
+    if shard == nil {
+        panic("shard is nil")
+    }
     shard.mutex.Lock()
     defer shard.mutex.Unlock()
 
-    _, existed := shard.table[key]
-
-    if existed {
-        return 0
-    } else {
-        // insert
-        shard.table[key] = val
-        return 1
+    node := shard.head
+    for node != nil {
+        if node.key == key {
+            node.val = val
+            return 1
+        }
+        node = node.next
     }
+    return 0
 }
 
 // return the number of updated key-value
-func (d *Dict)PutIfExists(key string, val interface{})int {
-    shard := d.shards[d.spread(key)]
+func (dict *Dict) PutIfExists(key string, val interface{})(result int) {
+    if dict == nil {
+        panic("dict is nil")
+    }
+    hashCode := fnv32(key)
+    index := dict.spread(hashCode)
+
+    rehashIndex := atomic.LoadInt32(&dict.rehashIndex)
+    if rehashIndex >= int32(index) {
+        dict.ensureNextTable()
+        nextShard := dict.getNextShard(hashCode)
+        result = nextShard.PutIfExists(key, val)
+    } else {
+        shard := dict.getShard(index)
+        result = shard.PutIfExists(key, val)
+    }
+    return
+}
+
+func (shard *Shard) Remove(key string) int {
+    if shard == nil {
+        panic("shard is nil")
+    }
     shard.mutex.Lock()
     defer shard.mutex.Unlock()
 
-    _, existed := shard.table[key]
-
-    if !existed {
+    node := shard.head
+    if node == nil {
+        // empty shard
         return 0
-    } else {
-        // update
-        shard.table[key] = val
+    } else if node.key == key {
+        // remove first node
+        shard.head = node.next
         return 1
+    } else {
+        prev := node
+        node = node.next
+        for node != nil {
+            if node.key == key {
+                prev.next = node.next
+                return 1
+            }
+            prev = node
+            node = node.next
+        }
     }
+    return 0
 }
 
 // return the number of deleted key-value
-func (d *Dict)Remove(key string)int {
-    shard := d.shards[d.spread(key)]
-    shard.mutex.Lock()
-    defer shard.mutex.Unlock()
-
-    _, existed := shard.table[key]
-    delete(shard.table, key)
-
-    if existed {
-        atomic.AddInt32(&d.count, -1)
-        return 1
-    } else {
-        return 0
+func (dict *Dict) Remove(key string)(result int) {
+    if dict == nil {
+        panic("dict is nil")
     }
+    hashCode := fnv32(key)
+    index := dict.spread(hashCode)
+    rehashIndex := atomic.LoadInt32(&dict.rehashIndex)
+    if rehashIndex >= int32(index) {
+        dict.ensureNextTable()
+        nextShard := dict.getNextShard(hashCode)
+        result = nextShard.Remove(key)
+    } else {
+        shard := dict.getShard(dict.spread(hashCode))
+        result = shard.Remove(key)
+    }
+    if result > 0 {
+        atomic.AddInt32(&dict.count, -1)
+    }
+    return
 }
 
+func (dict *Dict) addCount() int32 {
+    count := atomic.AddInt32(&dict.count, 1)
+    table, _ := dict.table.Load().([]*Shard)
+    if float64(count) >= float64(len(table))*loadFactor {
+       dict.resize()
+    }
+    return count
+}
+
+func (dict *Dict) resize() {
+    if !atomic.CompareAndSwapInt32(&dict.rehashIndex, -1, 0) {
+        // resize already in progress
+        return
+    }
+    dict.ensureNextTable()
+
+    var wg sync.WaitGroup
+    wg.Add(rehashConcurrent)
+    for i := 0; i < rehashConcurrent; i++ {
+        go dict.transfer(&wg)
+    }
+    wg.Wait()
+
+    // finish rehash
+    dict.table.Store(dict.nextTable)
+    dict.nextTable = nil
+    atomic.StoreInt32(&dict.rehashIndex, -1)
+}
+
+func (dict *Dict) transfer(wg *sync.WaitGroup) {
+    table, _ := dict.table.Load().([]*Shard)
+    tableSize := uint32(len(table))
+    // dict.rehashIndex must >= 0
+    for {
+        i := uint32(atomic.AddInt32(&dict.rehashIndex, 1)) - 1
+        if i >= tableSize {
+            wg.Done()
+            return
+        }
+        shard := dict.getShard(i)
+        shard.mutex.RLock()
+
+        nextShard0 := dict.nextTable[i]
+        nextShard1 := dict.nextTable[i+tableSize]
+
+        nextShard0.mutex.RLock()
+        nextShard1.mutex.RLock()
+
+        var head0, head1 *Node
+        var tail0, tail1 *Node
+        node := shard.head
+        for node != nil {
+            // split current shard to 2 shards in next table
+            if node.hashCode&tableSize == 0 {
+                if head0 == nil {
+                    head0 = node
+                } else {
+                    tail0.next = node
+                }
+                tail0 = node
+            } else {
+                if head1 == nil {
+                    head1 = node
+                } else {
+                    tail1.next = node
+                }
+                tail1 = node
+            }
+            node = node.next
+        }
+
+        if tail0 != nil {
+            tail0.next = nil
+
+            nextShard0.head = head0
+        }
+
+        if tail1 != nil {
+            tail1.next = nil
+
+            nextShard1.head = head1
+        }
+
+        nextShard1.mutex.RUnlock()
+        nextShard0.mutex.RUnlock()
+        shard.mutex.RUnlock()
+    }
+}
--- src/datastruct/dict/dict_test.go
+++ src/datastruct/dict/dict_test.go
@@ -1,115 +1,221 @@
 package dict
 
 import (
-    "testing"
     "strconv"
+    "sync"
+    "testing"
+    "time"
 )
 
 func TestPut(t *testing.T) {
     d := Make(0)
-
-    // insert
-    ret := d.Put("a", 1)
-    if ret != 1 { // insert 1
-        t.Error("put test failed: expected result 1, actual: " + strconv.Itoa(ret))
-    }
-    val, ok := d.Get("a")
-    if ok {
-        intVal, _ := val.(int)
-        if intVal != 1 {
-            t.Error("put test failed: expected 2, actual: " + strconv.Itoa(intVal))
+    count := 100
+    var wg sync.WaitGroup
+    wg.Add(count)
+    for i := 0; i < count; i++ {
+        // insert
+        key := "k" + strconv.Itoa(i)
+        ret := d.Put(key, i)
+        if ret != 1 { // insert 1
+            t.Error("put test failed: expected result 1, actual: " + strconv.Itoa(ret) + ", key: " + key)
         }
-    } else {
-        t.Error("put test failed: expected true, actual: false")
-    }
-
-    // update
-    ret = d.Put("a", 2)
-    if ret != 0 { // no insert
-        t.Error("put test failed: expected result 0, actual: " + strconv.Itoa(ret))
-    }
-    val, ok = d.Get("a")
-    if ok {
-        intVal, _ := val.(int)
-        if intVal != 2 {
-            t.Error("put test failed: expected 2, actual: " + strconv.Itoa(intVal))
+        val, ok := d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != i {
+                t.Error("put test failed: expected " + strconv.Itoa(i) + ", actual: " + strconv.Itoa(intVal) + ", key: " + key)
+            }
+        } else {
+            time.Sleep(2 * time.Second)
+            val2, ok2 := d.Get(key)
+            intVal2, _ := val2.(int)
+            t.Error("put test failed: expected true, actual: false, key: " + key +
+                ", retry: " + strconv.FormatBool(ok2) + ", val: " + strconv.Itoa(intVal2))
         }
-    } else {
-        t.Error("put test failed: expected true, actual: false")
+
+        // update
+        ret = d.Put(key, i*10)
+        if ret != 0 { // no insert
+            t.Error("put test failed: expected result 0, actual: " + strconv.Itoa(ret) + ", key: " + key)
+        }
+        val, ok = d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != i*10 {
+                t.Error("put test failed: expected " + strconv.Itoa(i*10) + ", actual: " + strconv.Itoa(intVal) + ", key: " + key)
+            }
+        } else {
+            t.Error("put test failed: expected true, actual: false, key: " + key)
+        }
     }
 }
 
 func TestPutIfAbsent(t *testing.T) {
     d := Make(0)
 
-    // insert
-    ret := d.PutIfAbsent("a", 1)
-    if ret != 1 { // insert 1
-        t.Error("put test failed: expected result 1, actual: " + strconv.Itoa(ret))
-    }
-    val, ok := d.Get("a")
-    if ok {
-        intVal, _ := val.(int)
-        if intVal != 1 {
-            t.Error("put test failed: expected 2, actual: " + strconv.Itoa(intVal))
+    for i := 0; i < 100; i++ {
+        // insert
+        key := "k" + strconv.Itoa(i)
+        ret := d.PutIfAbsent(key, i)
+        if ret != 1 { // insert 1
+            t.Error("put test failed: expected result 1, actual: " + strconv.Itoa(ret))
         }
-    } else {
-        t.Error("put test failed: expected true, actual: false")
-    }
+        val, ok := d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != i {
+                t.Error("put test failed: expected " + strconv.Itoa(i) + ", actual: " + strconv.Itoa(intVal))
+            }
+        } else {
+            t.Error("put test failed: expected true, actual: false")
+        }
 
-    // update
-    ret = d.PutIfAbsent("a", 2)
-    if ret != 0 { // no update
-        t.Error("put test failed: expected result 0, actual: " + strconv.Itoa(ret))
-    }
-    val, ok = d.Get("a")
-    if ok {
-        intVal, _ := val.(int)
-        if intVal != 1 {
-            t.Error("put test failed: expected 2, actual: " + strconv.Itoa(intVal))
+        // update
+        ret = d.PutIfAbsent(key, i * 10)
+        if ret != 0 { // no update
+            t.Error("put test failed: expected result 0, actual: " + strconv.Itoa(ret))
         }
-    } else {
-        t.Error("put test failed: expected true, actual: false")
+        val, ok = d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != i {
+                t.Error("put test failed: expected " + strconv.Itoa(i) + ", actual: " + strconv.Itoa(intVal))
+            }
+        } else {
+            t.Error("put test failed: expected true, actual: false")
+        }
     }
 }
 
 func TestPutIfExists(t *testing.T) {
     d := Make(0)
 
-    // insert
-    ret := d.PutIfExists("a", 1)
-    if ret != 0 { // insert
-        t.Error("put test failed: expected result 0, actual: " + strconv.Itoa(ret))
-    }
-
-    d.Put("a", 1)
-    ret = d.PutIfExists("a", 2)
-    val, ok := d.Get("a")
-    if ok {
-        intVal, _ := val.(int)
-        if intVal != 2 {
-            t.Error("put test failed: expected 2, actual: " + strconv.Itoa(intVal))
+    for i := 0; i < 100; i++ {
+        // insert
+        key := "k" + strconv.Itoa(i)
+        // insert
+        ret := d.PutIfExists(key, i)
+        if ret != 0 { // insert
+            t.Error("put test failed: expected result 0, actual: " + strconv.Itoa(ret))
         }
-    } else {
-        t.Error("put test failed: expected true, actual: false")
+
+        d.Put(key, i)
+        ret = d.PutIfExists(key, 10 * i)
+        val, ok := d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != 10 * i {
+                t.Error("put test failed: expected " + strconv.Itoa(10 * i) + ", actual: " + strconv.Itoa(intVal))
+            }
+        } else {
+            t.Error("put test failed: expected true, actual: false")
+        }
     }
 }
 
 func TestRemove(t *testing.T) {
     d := Make(0)
 
-    // insert
-    d.Put("a", 1)
-    ret := d.Remove("a")
-    if ret != 1 {
-        t.Error("remove test failed: expected result 1, actual: " + strconv.Itoa(ret))
+    // remove head node
+    for i := 0; i < 100; i++ {
+        // insert
+        key := "k" + strconv.Itoa(i)
+        d.Put(key, i)
     }
-    _, ok := d.Get("a")
-    if ok {
-        t.Error("remove test failed: expected true, actual false")
+    for i := 0; i < 100; i++ {
+        key := "k" + strconv.Itoa(i)
+
+        val, ok := d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != i {
+                t.Error("put test failed: expected " + strconv.Itoa(i) + ", actual: " + strconv.Itoa(intVal))
+            }
+        } else {
+            t.Error("put test failed: expected true, actual: false")
+        }
+
+        ret := d.Remove(key)
+        if ret != 1 {
+            t.Error("remove test failed: expected result 1, actual: " + strconv.Itoa(ret) + ", key:" + key)
+        }
+        _, ok = d.Get(key)
+        if ok {
+            t.Error("remove test failed: expected true, actual false")
+        }
+        ret = d.Remove(key)
+        if ret != 0 {
+            t.Error("remove test failed: expected result 0 actual: " + strconv.Itoa(ret))
+        }
     }
-    ret = d.Remove("a")
-    if ret != 0 {
-        t.Error("remove test failed: expected result 0 actual: " + strconv.Itoa(ret))
+
+    // remove tail node
+    d = Make(0)
+    for i := 0; i < 100; i++ {
+        // insert
+        key := "k" + strconv.Itoa(i)
+        d.Put(key, i)
+    }
+    for i := 9; i >= 0; i-- {
+        key := "k" + strconv.Itoa(i)
+
+        val, ok := d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != i {
+                t.Error("put test failed: expected " + strconv.Itoa(i) + ", actual: " + strconv.Itoa(intVal))
+            }
+        } else {
+            t.Error("put test failed: expected true, actual: false")
+        }
+
+        ret := d.Remove(key)
+        if ret != 1 {
+            t.Error("remove test failed: expected result 1, actual: " + strconv.Itoa(ret))
+        }
+        _, ok = d.Get(key)
+        if ok {
+            t.Error("remove test failed: expected true, actual false")
+        }
+        ret = d.Remove(key)
+        if ret != 0 {
+            t.Error("remove test failed: expected result 0 actual: " + strconv.Itoa(ret))
+        }
+    }
+
+    // remove middle node
+    d = Make(0)
+    d.Put("head", 0)
+    for i := 0; i < 10; i++ {
+        // insert
+        key := "k" + strconv.Itoa(i)
+        d.Put(key, i)
+    }
+    d.Put("tail", 0)
+    for i := 9; i >= 0; i-- {
+        key := "k" + strconv.Itoa(i)
+
+        val, ok := d.Get(key)
+        if ok {
+            intVal, _ := val.(int)
+            if intVal != i {
+                t.Error("put test failed: expected " + strconv.Itoa(i) + ", actual: " + strconv.Itoa(intVal))
+            }
+        } else {
+            t.Error("put test failed: expected true, actual: false")
+        }
+
+        ret := d.Remove(key)
+        if ret != 1 {
+            t.Error("remove test failed: expected result 1, actual: " + strconv.Itoa(ret))
+        }
+        _, ok = d.Get(key)
+        if ok {
+            t.Error("remove test failed: expected true, actual false")
+        }
+        ret = d.Remove(key)
+        if ret != 0 {
+            t.Error("remove test failed: expected result 0 actual: " + strconv.Itoa(ret))
+        }
     }
 }
