e4e499d8c66e0bb24dfa77b5b5b934778af36002
hdt3213 <hdt3213@outlook.com>
2020-01-18 23:49:14 +0800

    bug fix

--- src/lib/logger/logger.go
+++ src/lib/logger/logger.go
@@ -1,90 +1,91 @@
 package logger
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "path/filepath"
-    "runtime"
-    "time"
-    "github.com/go-gin-demo/lib/files"
-    "io"
+	"fmt"
+	"github.com/HDT3213/godis/src/lib/files"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
 )
 
 type Settings struct {
-    Path string `yaml:"path"`
-    Name string `yaml:"name"`
-    Ext string  `yaml:"ext"`
-    TimeFormat string `yaml:"time-format"`
+	Path       string `yaml:"path"`
+	Name       string `yaml:"name"`
+	Ext        string `yaml:"ext"`
+	TimeFormat string `yaml:"time-format"`
 }
 
 var (
-    F *os.File
-    DefaultPrefix      = ""
-    DefaultCallerDepth = 2
-    logger     *log.Logger
-    logPrefix  = ""
-    levelFlags = []string{"DEBUG", "INFO", "WARN", "ERROR", "FATAL"}
+	F                  *os.File
+	DefaultPrefix      = ""
+	DefaultCallerDepth = 2
+	logger             *log.Logger
+	logPrefix          = ""
+	levelFlags         = []string{"DEBUG", "INFO", "WARN", "ERROR", "FATAL"}
 )
 
 type Level int
+
 const (
-    DEBUG   Level = iota
-    INFO
-    WARNING
-    ERROR
-    FATAL
+	DEBUG Level = iota
+	INFO
+	WARNING
+	ERROR
+	FATAL
 )
 
 func Setup(settings *Settings) {
-    var err error
-    dir := settings.Path
-    fileName := fmt.Sprintf("%s-%s.%s",
-        settings.Name,
-        time.Now().Format(settings.TimeFormat),
-        settings.Ext)
+	var err error
+	dir := settings.Path
+	fileName := fmt.Sprintf("%s-%s.%s",
+		settings.Name,
+		time.Now().Format(settings.TimeFormat),
+		settings.Ext)
 
-    logFile, err := files.MustOpen(fileName, dir)
-    if err != nil {
-        log.Fatalf("logging.Setup err: %s", err)
-    }
+	logFile, err := files.MustOpen(fileName, dir)
+	if err != nil {
+		log.Fatalf("logging.Setup err: %s", err)
+	}
 
-    mw := io.MultiWriter(os.Stdout, logFile)
-    logger = log.New(mw, DefaultPrefix, log.LstdFlags)
+	mw := io.MultiWriter(os.Stdout, logFile)
+	logger = log.New(mw, DefaultPrefix, log.LstdFlags)
 }
 
 func setPrefix(level Level) {
-    _, file, line, ok := runtime.Caller(DefaultCallerDepth)
-    if ok {
-        logPrefix = fmt.Sprintf("[%s][%s:%d] ", levelFlags[level], filepath.Base(file), line)
-    } else {
-        logPrefix = fmt.Sprint("[%s] ", levelFlags[level])
-    }
+	_, file, line, ok := runtime.Caller(DefaultCallerDepth)
+	if ok {
+		logPrefix = fmt.Sprintf("[%s][%s:%d] ", levelFlags[level], filepath.Base(file), line)
+	} else {
+		logPrefix = fmt.Sprint("[%s] ", levelFlags[level])
+	}
 
-    logger.SetPrefix(logPrefix)
+	logger.SetPrefix(logPrefix)
 }
 
 func Debug(v ...interface{}) {
-    setPrefix(DEBUG)
-    logger.Println(v)
+	setPrefix(DEBUG)
+	logger.Println(v)
 }
 
 func Info(v ...interface{}) {
-    setPrefix(INFO)
-    logger.Println(v)
+	setPrefix(INFO)
+	logger.Println(v)
 }
 
 func Warn(v ...interface{}) {
-    setPrefix(WARNING)
-    logger.Println(v)
+	setPrefix(WARNING)
+	logger.Println(v)
 }
 
 func Error(v ...interface{}) {
-    setPrefix(ERROR)
-    logger.Println(v)
+	setPrefix(ERROR)
+	logger.Println(v)
 }
 
 func Fatal(v ...interface{}) {
-    setPrefix(FATAL)
-    logger.Fatalln(v)
+	setPrefix(FATAL)
+	logger.Fatalln(v)
 }
