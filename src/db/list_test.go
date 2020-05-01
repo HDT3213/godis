4289d621f66cab985436b0fb30779f256d264f32
hdt3213 <hdt3213@outlook.com>
2020-05-01 17:00:33 +0800

    list test

---
+++ src/db/list_test.go
@@ -0,0 +1,214 @@
+package db
+
+import (
+    "fmt"
+    "github.com/HDT3213/godis/src/datastruct/utils"
+    "github.com/HDT3213/godis/src/redis/reply"
+    "math/rand"
+    "strconv"
+    "testing"
+)
+
+func TestPush(t *testing.T) {
+    FlushAll(testDB, [][]byte{})
+    size := 100
+
+    // rpush single
+    key := strconv.FormatInt(int64(rand.Int()), 10)
+    values := make([][]byte, size)
+    for i := 0; i < size; i++ {
+        value := strconv.FormatInt(int64(rand.Int()), 10)
+        values[i] = []byte(value)
+        result, _ := RPush(testDB, toArgs(key, value))
+        if intResult, _ := result.(*reply.IntReply); intResult.Code != int64(i+1) {
+            t.Error(fmt.Sprintf("expected %d, actually %d", i+1, intResult.Code))
+        }
+    }
+    actual, _ := LRange(testDB, toArgs(key, "0", "-1"))
+    expected := reply.MakeMultiBulkReply(values)
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error("push error")
+    }
+    Del(testDB, toArgs(key))
+
+    // rpush multi
+    key = strconv.FormatInt(int64(rand.Int()), 10)
+    values = make([][]byte, size+1)
+    values[0] = []byte(key)
+    for i := 0; i < size; i++ {
+        value := strconv.FormatInt(int64(rand.Int()), 10)
+        values[i+1] = []byte(value)
+    }
+    result, _ := RPush(testDB, values)
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != int64(size) {
+        t.Error(fmt.Sprintf("expected %d, actually %d", size, intResult.Code))
+    }
+    actual, _ = LRange(testDB, toArgs(key, "0", "-1"))
+    expected = reply.MakeMultiBulkReply(values[1:])
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error("push error")
+    }
+    Del(testDB, toArgs(key))
+
+    // left push single
+    key = strconv.FormatInt(int64(rand.Int()), 10)
+    values = make([][]byte, size)
+    for i := 0; i < size; i++ {
+        value := strconv.FormatInt(int64(rand.Int()), 10)
+        values[size-i-1] = []byte(value)
+        result, _ = LPush(testDB, toArgs(key, value))
+        if intResult, _ := result.(*reply.IntReply); intResult.Code != int64(i+1) {
+            t.Error(fmt.Sprintf("expected %d, actually %d", i+1, intResult.Code))
+        }
+    }
+    actual, _ = LRange(testDB, toArgs(key, "0", "-1"))
+    expected = reply.MakeMultiBulkReply(values)
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error("push error")
+    }
+    Del(testDB, toArgs(key))
+
+    // left push multi
+    key = strconv.FormatInt(int64(rand.Int()), 10)
+    values = make([][]byte, size+1)
+    values[0] = []byte(key)
+    expectedValues := make([][]byte, size)
+    for i := 0; i < size; i++ {
+        value := strconv.FormatInt(int64(rand.Int()), 10)
+        values[i+1] = []byte(value)
+        expectedValues[size-i-1] = []byte(value)
+    }
+    result, _ = LPush(testDB, values)
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != int64(size) {
+        t.Error(fmt.Sprintf("expected %d, actually %d", size, intResult.Code))
+    }
+    actual, _ = LRange(testDB, toArgs(key, "0", "-1"))
+    expected = reply.MakeMultiBulkReply(expectedValues)
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error("push error")
+    }
+    Del(testDB, toArgs(key))
+}
+
+func TestLRange(t *testing.T) {
+    // prepare list
+    FlushAll(testDB, [][]byte{})
+    size := 100
+    key := strconv.FormatInt(int64(rand.Int()), 10)
+    values := make([][]byte, size)
+    for i := 0; i < size; i++ {
+        value := strconv.FormatInt(int64(rand.Int()), 10)
+        RPush(testDB, toArgs(key, value))
+        values[i] = []byte(value)
+    }
+
+    start := "0"
+    end := "9"
+    actual, _ := LRange(testDB, toArgs(key, start, end))
+    expected := reply.MakeMultiBulkReply(values[0:10])
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error(fmt.Sprintf("range error [%s, %s]", start, end))
+    }
+
+    start = "0"
+    end = "200"
+    actual, _ = LRange(testDB, toArgs(key, start, end))
+    expected = reply.MakeMultiBulkReply(values)
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error(fmt.Sprintf("range error [%s, %s]", start, end))
+    }
+
+    start = "0"
+    end = "-10"
+    actual, _ = LRange(testDB, toArgs(key, start, end))
+    expected = reply.MakeMultiBulkReply(values[0 : size-10+1])
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error(fmt.Sprintf("range error [%s, %s]", start, end))
+    }
+
+    start = "0"
+    end = "-200"
+    actual, _ = LRange(testDB, toArgs(key, start, end))
+    expected = reply.MakeMultiBulkReply(values[0:0])
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error(fmt.Sprintf("range error [%s, %s]", start, end))
+    }
+
+    start = "-10"
+    end = "-1"
+    actual, _ = LRange(testDB, toArgs(key, start, end))
+    expected = reply.MakeMultiBulkReply(values[90:])
+    if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
+        t.Error(fmt.Sprintf("range error [%s, %s]", start, end))
+    }
+}
+
+func TestLIndex(t *testing.T) {
+    // prepare list
+    FlushAll(testDB, [][]byte{})
+    size := 100
+    key := strconv.FormatInt(int64(rand.Int()), 10)
+    values := make([][]byte, size)
+    for i := 0; i < size; i++ {
+        value := strconv.FormatInt(int64(rand.Int()), 10)
+        RPush(testDB, toArgs(key, value))
+        values[i] = []byte(value)
+    }
+
+    result, _ := LLen(testDB, toArgs(key))
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != int64(size) {
+        t.Error(fmt.Sprintf("expected %d, actually %d", size, intResult.Code))
+    }
+
+    for i := 0; i < size; i++ {
+        result, _ = LIndex(testDB, toArgs(key, strconv.Itoa(i)))
+        expected := reply.MakeBulkReply(values[i])
+        if !utils.BytesEquals(result.ToBytes(), expected.ToBytes()) {
+            t.Error(fmt.Sprintf("expected %s, actually %s", string(expected.ToBytes()), string(result.ToBytes())))
+        }
+    }
+
+    for i := 1; i <= size; i++ {
+        result, _ = LIndex(testDB, toArgs(key, strconv.Itoa(-i)))
+        expected := reply.MakeBulkReply(values[size-i])
+        if !utils.BytesEquals(result.ToBytes(), expected.ToBytes()) {
+            t.Error(fmt.Sprintf("expected %s, actually %s", string(expected.ToBytes()), string(result.ToBytes())))
+        }
+    }
+}
+
+func TestLRem(t *testing.T) {
+    // prepare list
+    FlushAll(testDB, [][]byte{})
+    key := strconv.FormatInt(int64(rand.Int()), 10)
+    values := []string{key, "a", "b", "a", "a", "c", "a", "a"}
+    RPush(testDB, toArgs(values...))
+
+    result, _ := LRem(testDB, toArgs(key, "1", "a"))
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != 1 {
+        t.Error(fmt.Sprintf("expected %d, actually %d", 1, intResult.Code))
+    }
+    result, _ = LLen(testDB, toArgs(key))
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != 6 {
+        t.Error(fmt.Sprintf("expected %d, actually %d", 6, intResult.Code))
+    }
+
+    result, _ = LRem(testDB, toArgs(key, "-2", "a"))
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != 2 {
+        t.Error(fmt.Sprintf("expected %d, actually %d", 2, intResult.Code))
+    }
+    result, _ = LLen(testDB, toArgs(key))
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != 4 {
+        t.Error(fmt.Sprintf("expected %d, actually %d", 4, intResult.Code))
+    }
+
+    result, _ = LRem(testDB, toArgs(key, "0", "a"))
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != 2 {
+        t.Error(fmt.Sprintf("expected %d, actually %d", 2, intResult.Code))
+    }
+    result, _ = LLen(testDB, toArgs(key))
+    if intResult, _ := result.(*reply.IntReply); intResult.Code != 2 {
+        t.Error(fmt.Sprintf("expected %d, actually %d", 2, intResult.Code))
+    }
+
+}
--- src/db/string_test.go
+++ src/db/string_test.go
@@ -8,49 +8,49 @@ import (
     "testing"
 )
 
-var db = makeTestDB()
+var testDB = makeTestDB()
 
 func TestSet(t *testing.T) {
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     key := strconv.FormatInt(int64(rand.Int()), 10)
     value := strconv.FormatInt(int64(rand.Int()), 10)
 
     // normal set
-    Set(db, toArgs(key, value))
-    actual, _ := Get(db, toArgs(key))
+    Set(testDB, toArgs(key, value))
+    actual, _ := Get(testDB, toArgs(key))
     expected := reply.MakeBulkReply([]byte(value))
     if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
         t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
     }
 
     // set nx
-    actual, _ = Set(db, toArgs(key, value, "NX"))
+    actual, _ = Set(testDB, toArgs(key, value, "NX"))
     if _, ok := actual.(*reply.NullBulkReply); !ok {
         t.Error("expected true actual false")
     }
 
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     key = strconv.FormatInt(int64(rand.Int()), 10)
     value = strconv.FormatInt(int64(rand.Int()), 10)
-    Set(db, toArgs(key, value, "NX"))
-    actual, _ = Get(db, toArgs(key))
+    Set(testDB, toArgs(key, value, "NX"))
+    actual, _ = Get(testDB, toArgs(key))
     expected = reply.MakeBulkReply([]byte(value))
     if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
         t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
     }
 
     // set xx
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     key = strconv.FormatInt(int64(rand.Int()), 10)
     value = strconv.FormatInt(int64(rand.Int()), 10)
-    actual, _ = Set(db, toArgs(key, value, "XX"))
+    actual, _ = Set(testDB, toArgs(key, value, "XX"))
     if _, ok := actual.(*reply.NullBulkReply); !ok {
         t.Error("expected true actually false ")
     }
 
-    Set(db, toArgs(key, value))
-    Set(db, toArgs(key, value, "XX"))
-    actual, _ = Get(db, toArgs(key))
+    Set(testDB, toArgs(key, value))
+    Set(testDB, toArgs(key, value, "XX"))
+    actual, _ = Get(testDB, toArgs(key))
     expected = reply.MakeBulkReply([]byte(value))
     if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
         t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
@@ -59,17 +59,17 @@ func TestSet(t *testing.T) {
 }
 
 func TestSetNX(t *testing.T) {
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     key := strconv.FormatInt(int64(rand.Int()), 10)
     value := strconv.FormatInt(int64(rand.Int()), 10)
-    SetNX(db, toArgs(key, value))
-    actual, _ := Get(db, toArgs(key))
+    SetNX(testDB, toArgs(key, value))
+    actual, _ := Get(testDB, toArgs(key))
     expected := reply.MakeBulkReply([]byte(value))
     if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
         t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
     }
 
-    actual, _ = SetNX(db, toArgs(key, value))
+    actual, _ = SetNX(testDB, toArgs(key, value))
     expected2 := reply.MakeIntReply(int64(0))
     if !utils.BytesEquals(actual.ToBytes(), expected2.ToBytes()) {
         t.Error("expected: " + string(expected2.ToBytes()) + ", actual: " + string(actual.ToBytes()))
@@ -77,13 +77,13 @@ func TestSetNX(t *testing.T) {
 }
 
 func TestSetEX(t *testing.T) {
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     key := strconv.FormatInt(int64(rand.Int()), 10)
     value := strconv.FormatInt(int64(rand.Int()), 10)
     ttl := "1000"
 
-    SetEX(db, toArgs(key, ttl, value))
-    actual, _ := Get(db, toArgs(key))
+    SetEX(testDB, toArgs(key, ttl, value))
+    actual, _ := Get(testDB, toArgs(key))
     expected2 := reply.MakeBulkReply([]byte(value))
     if !utils.BytesEquals(actual.ToBytes(), expected2.ToBytes()) {
         t.Error("expected: " + string(expected2.ToBytes()) + ", actual: " + string(actual.ToBytes()))
@@ -91,7 +91,7 @@ func TestSetEX(t *testing.T) {
 }
 
 func TestMSet(t *testing.T) {
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     size := 10
     keys := make([]string, size)
     values := make([][]byte, size)
@@ -102,8 +102,8 @@ func TestMSet(t *testing.T) {
         values[i] = []byte(value)
         args = append(args, keys[i], value)
     }
-    MSet(db, toArgs(args...))
-    actual, _ := MGet(db, toArgs(keys...))
+    MSet(testDB, toArgs(args...))
+    actual, _ := MGet(testDB, toArgs(keys...))
     expected := reply.MakeMultiBulkReply(values)
     if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
         t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
@@ -111,39 +111,39 @@ func TestMSet(t *testing.T) {
 }
 
 func TestIncr(t *testing.T) {
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     size := 10
     key := strconv.FormatInt(int64(rand.Int()), 10)
     for i := 0; i < size; i++ {
-        Incr(db, toArgs(key))
-        actual, _ := Get(db, toArgs(key))
+        Incr(testDB, toArgs(key))
+        actual, _ := Get(testDB, toArgs(key))
         expected := reply.MakeBulkReply([]byte(strconv.FormatInt(int64(i+1), 10)))
         if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
             t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
         }
     }
     for i := 0; i < size; i++ {
-        IncrBy(db, toArgs(key, "-1"))
-        actual, _ := Get(db, toArgs(key))
+        IncrBy(testDB, toArgs(key, "-1"))
+        actual, _ := Get(testDB, toArgs(key))
         expected := reply.MakeBulkReply([]byte(strconv.FormatInt(int64(size-i-1), 10)))
         if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
             t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
         }
     }
 
-    FlushAll(db, [][]byte{})
+    FlushAll(testDB, [][]byte{})
     key = strconv.FormatInt(int64(rand.Int()), 10)
     for i := 0; i < size; i++ {
-        IncrBy(db, toArgs(key, "1"))
-        actual, _ := Get(db, toArgs(key))
+        IncrBy(testDB, toArgs(key, "1"))
+        actual, _ := Get(testDB, toArgs(key))
         expected := reply.MakeBulkReply([]byte(strconv.FormatInt(int64(i+1), 10)))
         if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
             t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
         }
     }
     for i := 0; i < size; i++ {
-        IncrByFloat(db, toArgs(key, "-1.0"))
-        actual, _ := Get(db, toArgs(key))
+        IncrByFloat(testDB, toArgs(key, "-1.0"))
+        actual, _ := Get(testDB, toArgs(key))
         expected := reply.MakeBulkReply([]byte(strconv.FormatInt(int64(size-i-1), 10)))
         if !utils.BytesEquals(actual.ToBytes(), expected.ToBytes()) {
             t.Error("expected: " + string(expected.ToBytes()) + ", actual: " + string(actual.ToBytes()))
