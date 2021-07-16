5736345f0df07b5de06649569baea00a1a8eb78f
hdt3213 <hdt3213@outlook.com>
2021-07-16 13:08:26 +0800

    tiny bug fix

--- cmd/main.go
+++ cmd/main.go
@@ -35,7 +35,7 @@ func main() {
 	logger.Setup(&logger.Settings{
 		Path:       "logs",
 		Name:       "godis",
-		Ext:        ".log",
+		Ext:        "log",
 		TimeFormat: "2006-01-02",
 	})
 	configFilename := os.Getenv("CONFIG")
