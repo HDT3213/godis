103315226a8bf47284c1d1cd91d8a396be102428
zhengtianyi <zhengtianyi@sinkatec.com>
2022-11-03 10:59:23 +0800

    fix: uses the provided seed value to initialize the function rand, for fixing the generation of the same rand string in the case of multiple executions.
    Limited ability, make it better~

--- lib/utils/rand_string.go
+++ lib/utils/rand_string.go
@@ -1,14 +1,18 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 // RandString create a random string no longer than n
 func RandString(n int) string {
+	nR := rand.New(rand.NewSource(time.Now().UnixNano()))
 	b := make([]rune, n)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[nR.Intn(len(letters))]
 	}
 	return string(b)
 }
