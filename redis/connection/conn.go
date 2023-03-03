5bd40fa431ffd3b8ef1a3455b0cc3920d71f6710
bijingrui <bijingrui@126.com>
2023-03-03 11:12:53 +0800

    flagMaster means this a connection with master

--- redis/connection/conn.go
+++ redis/connection/conn.go
@@ -11,7 +11,7 @@ import (
 const (
 	// flagSlave means this a connection with slave
 	flagSlave = uint64(1 << iota)
-	// flagSlave means this a connection with master
+	// flagMaster means this a connection with master
 	flagMaster
 	// flagMulti means this connection is within a transaction
 	flagMulti
