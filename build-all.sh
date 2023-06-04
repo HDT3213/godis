527ed379f07cccb077b9c45529c2b082e8428046
finley <hdt3213@outlook.com>
2023-06-05 07:53:45 +0800

    add build for more dist

--- build-all.sh
+++ build-all.sh
@@ -1,2 +1,7 @@
-./build-darwin.sh
-./build-linux.sh
+#!/usr/bin/env bash
+
+CGO_ENABLED=0  GOOS=linux GOARCH=amd64 go build -o target/godis-linux-amd64 ./
+CGO_ENABLED=0  GOOS=linux GOARCH=arm64 go build -o target/godis-linux-arm64 ./
+CGO_ENABLED=0  GOOS=darwin GOARCH=amd64  go build -o target/godis-darwin-amd64 ./
+CGO_ENABLED=0  GOOS=darwin GOARCH=arm64  go build -o target/godis-darwin-arm64 ./
+CGO_ENABLED=0  GOOS=windows GOARCH=amd64  go build -o target/godis-windows-amd64.exe ./
